989ba8122b64ee0b774bed0d12b6ff878225b0d3
kli-cauc <603469018@qq.com>
2016-08-04 16:41:47 +0800

    Update isort.h

--- isort.h
+++ isort.h
@@ -1,8 +1,34 @@
 #ifndef ISORT_H
 #define ISORT_H
-void isort(vector<int> v,int low,int high){
-
+void isort(vector<int> &v,int low,int high){
+  //检查
+  if(low >= high){
+    return;
+  }
+  
+  //开始排序
+  for(unsigned i = 1; i < (high - low + 1); ++i){
+    if(v[i] >= v[i-1]){
+      continue;
+    }else{  //v[i] < v[i-1]
+      //开始插入
+      unsigned index = i; //暂存索引
+      do{
+        swap(v[i], v[i-1]);
+        i--;
+        
+        if(i == 0) //检查，防止越界
+          break;
+        
+      }while(v[i] < v[i-1]);
+      
+      i = index;  //还原索引
+      
+    }
+  }
+  
+  //搞定返回
+  return;
 }
-
 
 #endif
