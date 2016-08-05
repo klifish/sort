10f9134387a4fa6785d2c4b0cab472589114abcb
kli-cauc <603469018@qq.com>
2016-08-05 15:55:55 +0800

    Update qsort.h

--- qsort.h
+++ qsort.h
@@ -42,3 +42,9 @@ void Qsort(vector<int> &vi, int low, int high){
 }
 
 #endif
+
+/*
+这里有一些小的tips：
+1、如果传给函数的第二个和第三个参数定义为unsigned型，则函数体内还需添加对参数范围进行检查，因为第37行“first-1”有可能变成-1（参见：http://blog.csdn.net/jiangdf/article/details/8711151），
+此时传给函数必然出错。使用定义为int，算是个不经意的小技巧吧
+*/
