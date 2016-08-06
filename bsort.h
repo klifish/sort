68111c94d1b8b8c66faf551a7c7527489d1067cf
kli-cauc <603469018@qq.com>
2016-08-06 14:46:25 +0800

    Update bsort.h

--- bsort.h
+++ bsort.h
@@ -1,1 +1,24 @@
+/*
+冒泡排序
+*/
+
+#ifndef BSORT_H
+#define BSORT_H
+template<typename T>
+void Bsort(vector<T> &v,int first,int last){
+  if(first > last){
+    return;
+  }
+  
+  int count = 0;//记录外层循环次数
+  for(int i = first; i <= last; ++i){
+    for(int j = first + 1; j <= last - count; ++j){
+      if(v[j-1] > v[j]){
+        swap(v[j-1],v[j]);
+      }
+    }
+  }
+}
+
+#endif
 
